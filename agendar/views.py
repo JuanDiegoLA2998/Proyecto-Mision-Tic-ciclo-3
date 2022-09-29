f0a446fb5b92ee1f04be2e92cff17bedd5e3401a
unknown <andresfhv@outlook.com>
2022-09-28 19:19:28 -0500

    first commit

---
+++ agendar/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+def agenda(request):
+    return HttpResponse("Aquí se agenda xdxd")
+def principal(request):
+    return HttpResponse("Página principal")
+
+
+# Create your views here.
